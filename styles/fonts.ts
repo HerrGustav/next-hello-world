f79c931aebf2359e850386f3053c6f2fd43d9037
HerrGustav <juri.tafferner@gmail.com>
2022-08-09 22:16:50 +0200

    Add more docs

--- styles/fonts.ts
+++ styles/fonts.ts
@@ -6,8 +6,12 @@ const baseFontSize = 16;
 
 const pxToRem = (pxValue: number): number => pxValue / baseFontSize;
 
-// fontSize returns the font size of a given input in pixel as rem value.
-// This makes it easier to translate the figma design to the actual styling code.
+/**
+ * @param pxValue number
+ * @desc fontSize returns the font size of a given input in pixel as rem value.
+ * This makes it easier to translate the figma design to the actual styling code.
+ * @returns Css Properties
+ */
 const fontSize = (pxValue: number) =>
   css`
     font-size: ${pxToRem(pxValue)}rem;
--- styles/transitions.ts
+++ styles/transitions.ts
@@ -1,17 +1,34 @@
 import { css } from "styled-components";
 import { TransitionAnimationCurve, TransitionAnimationTiming } from "./global";
 
-// @TODO add a jest test:
+/**
+ *
+ * @param properties string | string[]
+ * @param duration number
+ * @param curve string
+ * @param delay number
+ * @desc transition is creating a transition for one or multiple properties.
+ * If multiple properties are given, it will apply the same duration, curve and delay for them.
+ * @TODO add a jest test ref.: https://github.com/HerrGustav/hello-world/issues/4
+ * @returns Css Properties
+ */
 const transition = (
   properties: string | string[],
   duration: number = TransitionAnimationTiming,
   curve: string = TransitionAnimationCurve,
   delay: number = 0
 ) => {
-  const props =
-    typeof properties === "string" ? properties : properties.join(", ");
+  if (typeof properties === "string") {
+    return css`
+      transition: ${properties} ${duration}s ${curve} ${delay}s;
+    `;
+  }
+
   return css`
-    transition: ${props} ${duration}s ${curve} ${delay}s;
+    transition: ${properties.join(", ")};
+    transition-duration: ${duration}s;
+    transition-timing-function: ${curve};
+    transition-delay: ${delay}s;
   `;
 };
 
